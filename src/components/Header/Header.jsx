04d135082f9cbbf86502d606dca7c225dffe99b7
emonhosen0130 <irfanmuhammad013090@gmail.com>
2024-04-01 11:42:21 +0600

    add to postDetails and userDetails

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
-
+import './Header.css'
 const Header = () => {
     return (
         <div>
             <h2>Navbar</h2>
             <nav>
-                <Link to="/home">Home</Link>
+                <span>My Website</span>
+                <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/contact">Contact US</Link>
+                <Link to="/posts">Posts</Link>
                 <Link to="/users">Users</Link>
             </nav>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,10 @@ import Home from './components/Home/Home.jsx';
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import Users from './components/Users/Users.jsx';
+import App from './App.jsx'
+import UserDetails from './components/UserDetails/UserDetails.jsx';
+import Posts from './components/Posts/Posts.jsx';
+import PostDetails from './components/PostDetails/PostDetails.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -23,8 +27,24 @@ const router = createBrowserRouter([
         element:<Contact></Contact>,
       },
       {
-        path:"/user",
+        path:"/users",
+        loader: ()=> fetch('https://jsonplaceholder.typicode.com/users'),
         element:<Users></Users>
+      },
+      {
+        path:'/user/:userId',
+        loader:({params})=> fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
+        element:<UserDetails></UserDetails>
+      },
+      {
+        path:'/posts',
+        loader:()=> fetch('https://jsonplaceholder.typicode.com/posts'),
+        element:<Posts></Posts>
+      },
+      {
+        path:'/post/:postId',
+        loader:({params})=>fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`),
+        element:<PostDetails></PostDetails>
       }
     ]
   },
